0827b49a47e92783a0a18e48860c02df12bd4f23
Alex Reinoso <ralexjessiel@gmail.com>
2021-06-10 17:11:26 -0500

    Formulario de inicio de sesion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 /*Componentes */
 import { LoginComponent } from './components/login/login.component';
@@ -23,7 +26,10 @@ import { LoginComponent } from './components/login/login.component';
     MatToolbarModule,
     MatIconModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
