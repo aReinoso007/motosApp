b1f0e0196b77a0ae609ef138e1722e8774018793
Alex Reinoso <ralexjessiel@gmail.com>
2021-06-11 15:37:15 -0500

    mrouta de dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
@@ -7,7 +8,7 @@ const routes: Routes = [
   {path:'', redirectTo:'login', pathMatch:'full'},
   { path: 'login', component: LoginComponent},
   /*Esto es lazy loading, cuando el usuario entra a esa ruta se carga el modulo*/
-  { path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(x => x.DashboardModule) },
+  { path: 'dashboard', component: DashboardComponent},
   /*Aqui mas luego toca crear pageNotFound */
   { path:'sign-up', component: SignUpComponent},
   { path:'**', redirectTo:'login', pathMatch:'full'}
