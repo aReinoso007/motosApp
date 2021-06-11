ae5efa5d3d8bb04e2048a6ab86ab200f85590721
Alex Reinoso <ralexjessiel@gmail.com>
2021-06-11 16:27:27 -0500

    registro

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  showPassword = false;
+  passwordToggleIcon = 'eye';
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.form = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', Validators.email],
+      password: ['', Validators.required],
+      password2: ['', Validators.required]
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+    if(this.passwordToggleIcon == 'eye'){
+      this.passwordToggleIcon = 'eye-off'
+    }else{
+      this.passwordToggleIcon = 'eye';
+    }
+  }
+
+  async register(){
+    let err = await this.authService.signUp(
+      this.form.value.nombre, 
+      this.form.value.email, 
+      this.form.value.password)
+      
   }
 
 }
